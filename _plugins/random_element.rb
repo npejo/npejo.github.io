b56a996e291e34d75155d72576979ae557252bde
Nikola Pejoski <npejo@odesk.com>
2015-03-26 03:46:13 +0100

    Initial commit of my blog

---
+++ _plugins/random_element.rb
@@ -0,0 +1,13 @@
+class RandomElementTag < Liquid::Tag
+
+   def initialize(tag_name, max, tokens)
+     super
+     @max = max.to_i
+   end
+
+   def render(context)
+     rand(@max).to_s
+   end
+end
+
+Liquid::Template.register_tag('random', RandomElementTag)
